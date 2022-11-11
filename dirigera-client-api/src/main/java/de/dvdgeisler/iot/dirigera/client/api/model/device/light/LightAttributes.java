6c3de99bfe572c0ee8e091c6e7f2590ecf207fed
David Geisler <dvdgeisler@googlemail.com>
2022-11-11 16:38:18 +0100

    Add startupOnOff and identifyPeriod fields to light devices

--- dirigera-client-api/src/main/java/de/dvdgeisler/iot/dirigera/client/api/model/device/light/LightAttributes.java
+++ dirigera-client-api/src/main/java/de/dvdgeisler/iot/dirigera/client/api/model/device/light/LightAttributes.java
@@ -2,9 +2,16 @@ package de.dvdgeisler.iot.dirigera.client.api.model.device.light;
 
 import com.fasterxml.jackson.annotation.JsonUnwrapped;
 import de.dvdgeisler.iot.dirigera.client.api.model.device.DeviceAttributes;
+import de.dvdgeisler.iot.dirigera.client.api.model.device.DeviceStartupMode;
+
+import java.time.LocalDateTime;
 
 public class LightAttributes extends DeviceAttributes {
     public String productCode;
+
+    public DeviceStartupMode startupOnOff;
+    public Integer identifyPeriod;
+    public LocalDateTime identifyStarted;
 
     @JsonUnwrapped
     public LightStateAttributes state;
@@ -12,9 +19,22 @@ public class LightAttributes extends DeviceAttributes {
     public LightAttributes() {
     }
 
-    public LightAttributes(final String model, final String manufacturer, final String firmwareVersion, final String hardwareVersion, final String serialNumber, final String productCode, final LightStateAttributes state) {
+    public LightAttributes(
+            final String model,
+            final String manufacturer,
+            final String firmwareVersion,
+            final String hardwareVersion,
+            final String serialNumber,
+            final String productCode,
+            final DeviceStartupMode startupOnOff,
+            final Integer identifyPeriod,
+            final LocalDateTime identifyStarted,
+            final LightStateAttributes state) {
         super(model, manufacturer, firmwareVersion, hardwareVersion, serialNumber);
         this.productCode = productCode;
+        this.startupOnOff = startupOnOff;
+        this.identifyPeriod = identifyPeriod;
+        this.identifyStarted = identifyStarted;
         this.state = state;
     }
 }
