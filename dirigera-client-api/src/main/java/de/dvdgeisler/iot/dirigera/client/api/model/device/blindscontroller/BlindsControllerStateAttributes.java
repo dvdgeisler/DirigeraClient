bd82f01b810b3effbe62a40ba410e1c827b2f58d
David Geisler <dvdgeisler@googlemail.com>
2022-11-20 23:08:49 +0100

    Add Blinds-Controller data model

---
+++ dirigera-client-api/src/main/java/de/dvdgeisler/iot/dirigera/client/api/model/device/blindscontroller/BlindsControllerStateAttributes.java
@@ -0,0 +1,13 @@
+package de.dvdgeisler.iot.dirigera.client.api.model.device.blindscontroller;
+
+import de.dvdgeisler.iot.dirigera.client.api.model.device.DeviceStateAttributes;
+
+public class BlindsControllerStateAttributes extends DeviceStateAttributes {
+
+    public BlindsControllerStateAttributes(final String customName) {
+        super(customName);
+    }
+
+    public BlindsControllerStateAttributes() {
+    }
+}
