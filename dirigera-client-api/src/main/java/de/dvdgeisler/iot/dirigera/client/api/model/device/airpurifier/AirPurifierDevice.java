bc1264acfbb86be80d3e32be8bf893f4c0d1b5fc
David Geisler <dvdgeisler@googlemail.com>
2022-11-20 23:07:21 +0100

    Rename configuration attributes variable

--- dirigera-client-api/src/main/java/de/dvdgeisler/iot/dirigera/client/api/model/device/airpurifier/AirPurifierDevice.java
+++ dirigera-client-api/src/main/java/de/dvdgeisler/iot/dirigera/client/api/model/device/airpurifier/AirPurifierDevice.java
@@ -4,7 +4,6 @@ import de.dvdgeisler.iot.dirigera.client.api.model.device.Device;
 import de.dvdgeisler.iot.dirigera.client.api.model.device.DeviceCapabilities;
 import de.dvdgeisler.iot.dirigera.client.api.model.device.DeviceCategory;
 import de.dvdgeisler.iot.dirigera.client.api.model.device.DeviceType;
-import de.dvdgeisler.iot.dirigera.client.api.model.deviceset.DeviceSet;
 
 import java.time.LocalDateTime;
 import java.util.List;
@@ -12,8 +11,8 @@ import java.util.List;
 
 public class AirPurifierDevice extends Device<AirPurifierAttributes, AirPurifierConfigurationAttributes> {
 
-    public AirPurifierDevice(final String id, final LocalDateTime createdAt, final Boolean isReachable, final LocalDateTime lastSeen, final AirPurifierAttributes attributes, final DeviceCapabilities capabilities, final List<String> remoteLinks, final AirPurifierConfigurationAttributes outletConfigurationAttributes) {
-        super(id, DeviceCategory.AIR_PURIFIER, DeviceType.AIR_PURIFIER, createdAt, isReachable, lastSeen, attributes, capabilities, remoteLinks, outletConfigurationAttributes);
+    public AirPurifierDevice(final String id, final LocalDateTime createdAt, final Boolean isReachable, final LocalDateTime lastSeen, final AirPurifierAttributes attributes, final DeviceCapabilities capabilities, final List<String> remoteLinks, final AirPurifierConfigurationAttributes configurationAttributes) {
+        super(id, DeviceCategory.AIR_PURIFIER, DeviceType.AIR_PURIFIER, createdAt, isReachable, lastSeen, attributes, capabilities, remoteLinks, configurationAttributes);
     }
 
     public AirPurifierDevice() {
