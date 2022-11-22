d8dd07621388604cc4cba1f27a690602113f032b
David Geisler <dvdgeisler@googlemail.com>
2022-11-22 20:03:39 +0100

    Add websockets example

---
+++ dirigera-client-examples/src/main/java/de/dvdgeisler/iot/dirigera/client/examples/websockets/WebSockets.java
@@ -0,0 +1,37 @@
+package de.dvdgeisler.iot.dirigera.client.examples.websockets;
+
+import de.dvdgeisler.iot.dirigera.client.api.DirigeraApi;
+import de.dvdgeisler.iot.dirigera.client.api.http.ClientApi;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import reactor.core.publisher.Flux;
+
+/**
+ * Lists all known devices
+ */
+@SpringBootApplication
+@ComponentScan(basePackageClasses = {DirigeraApi.class})
+public class WebSockets {
+    private final static Logger log = LoggerFactory.getLogger(WebSockets.class);
+
+    @Bean
+    public CommandLineRunner run(final DirigeraApi api, final ClientApi capi) {
+
+        return (String... args) -> {
+            api.pairIfRequired().block(); // pair gateway if required
+
+            capi.websocket();
+        };
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebSockets.class, args).close();
+    }
+
+
+}
