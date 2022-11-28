fcd7105e1e2bef1fe6df76ab51e29284949dfdfc
David Geisler <dvdgeisler@googlemail.com>
2022-11-29 00:04:18 +0100

    Set speaker's playback state

--- dirigera-client-api/src/main/java/de/dvdgeisler/iot/dirigera/client/api/SpeakerDeviceApi.java
+++ dirigera-client-api/src/main/java/de/dvdgeisler/iot/dirigera/client/api/SpeakerDeviceApi.java
@@ -2,57 +2,78 @@ package de.dvdgeisler.iot.dirigera.client.api;
 
 import de.dvdgeisler.iot.dirigera.client.api.http.ClientApi;
 import de.dvdgeisler.iot.dirigera.client.api.model.device.Device;
-import de.dvdgeisler.iot.dirigera.client.api.model.device.speaker.SpeakerAttributes;
-import de.dvdgeisler.iot.dirigera.client.api.model.device.speaker.SpeakerConfigurationAttributes;
-import de.dvdgeisler.iot.dirigera.client.api.model.device.speaker.SpeakerDevice;
-import de.dvdgeisler.iot.dirigera.client.api.model.device.speaker.SpeakerStateAttributes;
-import java.util.Optional;
+import de.dvdgeisler.iot.dirigera.client.api.model.device.speaker.*;
 import reactor.core.publisher.Mono;
+
+import java.util.Optional;
+
+import static de.dvdgeisler.iot.dirigera.client.api.model.device.speaker.SpeakerPlaybackState.*;
 
 @SuppressWarnings("OptionalUsedAsFieldOrParameterType")
 public class SpeakerDeviceApi extends DefaultDeviceApi<
-    SpeakerStateAttributes,
-    SpeakerAttributes,
-    SpeakerConfigurationAttributes,
-    SpeakerDevice> {
+        SpeakerStateAttributes,
+        SpeakerAttributes,
+        SpeakerConfigurationAttributes,
+        SpeakerDevice> {
 
-  public SpeakerDeviceApi(final ClientApi clientApi) {
-    super(clientApi);
-  }
+    public SpeakerDeviceApi(final ClientApi clientApi) {
+        super(clientApi);
+    }
 
-  @Override
-  protected boolean isInstance(final Device<?, ?> device) {
-    return device instanceof SpeakerDevice;
-  }
+    @Override
+    protected boolean isInstance(final Device<?, ?> device) {
+        return device instanceof SpeakerDevice;
+    }
 
-  public Mono<SpeakerDevice> mute(final SpeakerDevice device) {
-      return this.assertCapability(device, "isMuted")
-          .flatMap(d -> this.setStateAttribute(d, createStateAttribute(Optional.of(true), Optional.empty())));
-  }
+    public Mono<SpeakerDevice> mute(final SpeakerDevice device) {
+        return this.assertCapability(device, "isMuted")
+                .flatMap(d -> this.setStateAttribute(d, createStateAttribute(Optional.of(true), Optional.empty(), Optional.empty())));
+    }
 
-  public Mono<SpeakerDevice> unmute(final SpeakerDevice device) {
-    return this.assertCapability(device, "isMuted")
-        .flatMap(d -> this.setStateAttribute(d, createStateAttribute(Optional.of(false), Optional.empty())));
-  }
+    public Mono<SpeakerDevice> unmute(final SpeakerDevice device) {
+        return this.assertCapability(device, "isMuted")
+                .flatMap(d -> this.setStateAttribute(d, createStateAttribute(Optional.of(false), Optional.empty(), Optional.empty())));
+    }
 
-  public Mono<SpeakerDevice> setVolume(final SpeakerDevice device, final Integer volume) {
+    public Mono<SpeakerDevice> setVolume(final SpeakerDevice device, final Integer volume) {
 
-    if(volume < 0 || volume > 100)
-      return Mono.error(new IllegalArgumentException("Volume must be between 0 and 100"));
+        if (volume < 0 || volume > 100)
+            return Mono.error(new IllegalArgumentException("Volume must be between 0 and 100"));
 
-    return this.assertCapability(device, "volume")
-        .flatMap(d -> this.setStateAttribute(d, createStateAttribute(Optional.empty(), Optional.of(volume))));
-  }
+        return this.assertCapability(device, "volume")
+                .flatMap(d -> this.setStateAttribute(d, createStateAttribute(Optional.empty(), Optional.of(volume), Optional.empty())));
+    }
 
-  // TODO: If possible figure out how to start playback. Device does support it as can be seen in the output
+    public Mono<SpeakerDevice> setPlayback(final SpeakerDevice device, final SpeakerPlaybackState playbackState) {
+        return this.assertCapability(device, "playback")
+                .flatMap(d -> this.setStateAttribute(d, createStateAttribute(Optional.empty(), Optional.empty(), Optional.of(playbackState))));
+    }
 
-  private SpeakerStateAttributes createStateAttribute(Optional<Boolean> isMuted, Optional<Integer> volume) {
-    final SpeakerStateAttributes attributes = new SpeakerStateAttributes();
+    public Mono<SpeakerDevice> play(final SpeakerDevice device) {
+        return this.setPlayback(device, PLAYING);
+    }
 
-    isMuted.ifPresent(b -> attributes.isMuted = b);
+    public Mono<SpeakerDevice> pause(final SpeakerDevice device) {
+        return this.setPlayback(device, PAUSED);
+    }
 
-    volume.ifPresent(i -> attributes.volume = i);
+    public Mono<SpeakerDevice> next(final SpeakerDevice device) {
+        return this.setPlayback(device, NEXT);
+    }
 
-    return attributes;
-  }
+    public Mono<SpeakerDevice> previous(final SpeakerDevice device) {
+        return this.setPlayback(device, PREVIOUS);
+    }
+
+    private SpeakerStateAttributes createStateAttribute(final Optional<Boolean> isMuted, final Optional<Integer> volume, final Optional<SpeakerPlaybackState> playback) {
+        final SpeakerStateAttributes attributes = new SpeakerStateAttributes();
+
+        isMuted.ifPresent(b -> attributes.isMuted = b);
+
+        volume.ifPresent(i -> attributes.volume = i);
+
+        playback.ifPresent(p -> attributes.playback = p);
+
+        return attributes;
+    }
 }
