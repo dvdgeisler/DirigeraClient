82673d5c2ec3dcfa9d6296fdec48b8233ec671c6
David Geisler <dvdgeisler@googlemail.com>
2022-11-29 00:37:22 +0100

    Add playList field to SpeakerAudioAttributes

--- dirigera-client-api/src/main/java/de/dvdgeisler/iot/dirigera/client/api/model/device/speaker/SpeakerAudioAttributes.java
+++ dirigera-client-api/src/main/java/de/dvdgeisler/iot/dirigera/client/api/model/device/speaker/SpeakerAudioAttributes.java
@@ -3,22 +3,24 @@ package de.dvdgeisler.iot.dirigera.client.api.model.device.speaker;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonInclude.Include;
 import de.dvdgeisler.iot.dirigera.client.api.model.music.MusicPlayItem;
+import de.dvdgeisler.iot.dirigera.client.api.model.music.MusicPlayList;
 
 @JsonInclude(Include.NON_NULL)
 public class SpeakerAudioAttributes {
-  public String serviceType;
-  public String providerType;
-  public MusicPlayItem playItem;
-  public MusicPlayItem nextPlayItem;
+    public String serviceType;
+    public String providerType;
+    public MusicPlayItem playItem;
+    public MusicPlayItem nextPlayItem;
+    public MusicPlayList playList;
 
-  public SpeakerAudioAttributes() {
-  }
+    public SpeakerAudioAttributes() {
+    }
 
-  public SpeakerAudioAttributes(String serviceType, String providerType, MusicPlayItem playItem,
-      MusicPlayItem nextPlayItem) {
-    this.serviceType = serviceType;
-    this.providerType = providerType;
-    this.playItem = playItem;
-    this.nextPlayItem = nextPlayItem;
-  }
+    public SpeakerAudioAttributes(final String serviceType, final String providerType, final MusicPlayItem playItem, final MusicPlayItem nextPlayItem, final MusicPlayList playList) {
+        this.serviceType = serviceType;
+        this.providerType = providerType;
+        this.playItem = playItem;
+        this.nextPlayItem = nextPlayItem;
+        this.playList = playList;
+    }
 }
