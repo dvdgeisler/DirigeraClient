5803841dcbf373050b99aaf97215fe831edc6889
David Geisler <dvdgeisler@googlemail.com>
2022-11-29 00:59:47 +0100

    Move field playbackLastChangedTimestamp to SpeakerAttributes

--- dirigera-client-api/src/main/java/de/dvdgeisler/iot/dirigera/client/api/model/device/speaker/SpeakerAttributes.java
+++ dirigera-client-api/src/main/java/de/dvdgeisler/iot/dirigera/client/api/model/device/speaker/SpeakerAttributes.java
@@ -8,25 +8,17 @@ public class SpeakerAttributes extends DeviceAttributes<SpeakerStateAttributes> 
     public Integer identifyPeriod;
     public LocalDateTime identifyStarted;
     public String audioGroup;
+    public LocalDateTime playbackLastChangedTimestamp;
 
     public SpeakerAttributes() {
     }
 
-    public SpeakerAttributes(
-        final String model,
-        final String manufacturer,
-        final String firmwareVersion,
-        final String hardwareVersion,
-        final String serialNumber,
-        final String productCode,
-        final Integer identifyPeriod,
-        final LocalDateTime identifyStarted,
-        final String audioGroup,
-        final SpeakerStateAttributes state) {
+    public SpeakerAttributes(final String model, final String manufacturer, final String firmwareVersion, final String hardwareVersion, final String serialNumber, final SpeakerStateAttributes state, final String productCode, final Integer identifyPeriod, final LocalDateTime identifyStarted, final String audioGroup, final LocalDateTime playbackLastChangedTimestamp) {
         super(model, manufacturer, firmwareVersion, hardwareVersion, serialNumber, state);
         this.productCode = productCode;
         this.identifyPeriod = identifyPeriod;
         this.identifyStarted = identifyStarted;
         this.audioGroup = audioGroup;
+        this.playbackLastChangedTimestamp = playbackLastChangedTimestamp;
     }
 }
--- dirigera-client-api/src/main/java/de/dvdgeisler/iot/dirigera/client/api/model/device/speaker/SpeakerStateAttributes.java
+++ dirigera-client-api/src/main/java/de/dvdgeisler/iot/dirigera/client/api/model/device/speaker/SpeakerStateAttributes.java
@@ -4,12 +4,9 @@ import com.fasterxml.jackson.annotation.JsonInclude;
 import de.dvdgeisler.iot.dirigera.client.api.model.device.DeviceStateAttributes;
 import de.dvdgeisler.iot.dirigera.client.api.model.music.MusicPlaybackState;
 
-import java.time.LocalDateTime;
-
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public class SpeakerStateAttributes extends DeviceStateAttributes {
   public MusicPlaybackState playback;
-  public LocalDateTime playbackLastChangedTimestamp;
   public SpeakerAudioAttributes playbackAudio;
   public SpeakerPlaybackPosition playbackPosition;
   public Integer volume;
