e74730f2f9fa6ed1a9d914a2e9d323b42d1f19d4
David Geisler <dvdgeisler@googlemail.com>
2022-12-12 22:00:14 +0100

    Remove unnecessary exception declaration from constructor

--- dirigera-client-api/src/main/java/de/dvdgeisler/iot/dirigera/client/api/http/homekit/AbstractHomekitApi.java
+++ dirigera-client-api/src/main/java/de/dvdgeisler/iot/dirigera/client/api/http/homekit/AbstractHomekitApi.java
@@ -3,11 +3,9 @@ package de.dvdgeisler.iot.dirigera.client.api.http.homekit;
 import de.dvdgeisler.iot.dirigera.client.api.http.HTTPClientApi;
 import de.dvdgeisler.iot.dirigera.client.api.mdns.RestApiDiscovery;
 
-import javax.net.ssl.SSLException;
+public abstract class AbstractHomekitApi extends HTTPClientApi {
 
-public abstract class AbstractClientApi extends HTTPClientApi {
-
-    public AbstractClientApi(final RestApiDiscovery discovery, final String path) throws SSLException {
+    public AbstractHomekitApi(final RestApiDiscovery discovery, final String path) {
         super(discovery, path, null);
     }
 }
