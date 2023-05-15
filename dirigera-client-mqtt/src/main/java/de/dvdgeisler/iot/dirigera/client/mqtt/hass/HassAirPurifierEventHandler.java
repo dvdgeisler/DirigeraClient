85161e08ca1e9e0f6de34a16f6df1b8174e9401a
TheMrBooyah <remco_van_herk@hotmail.com>
2023-05-15 19:29:59 +0200

    #79 An attempt to create more functionality for the AirPurifier inside home assistant

--- dirigera-client-mqtt/src/main/java/de/dvdgeisler/iot/dirigera/client/mqtt/hass/HassAirPurifierEventHandler.java
+++ dirigera-client-mqtt/src/main/java/de/dvdgeisler/iot/dirigera/client/mqtt/hass/HassAirPurifierEventHandler.java
@@ -8,22 +8,24 @@ import de.dvdgeisler.iot.dirigera.client.api.model.device.airpurifier.AirPurifie
 import de.dvdgeisler.iot.dirigera.client.mqtt.hass.model.DeviceAvailability;
 import de.dvdgeisler.iot.dirigera.client.mqtt.hass.model.DeviceAvailabilityState;
 import de.dvdgeisler.iot.dirigera.client.mqtt.hass.model.airpurifier.AirPurifierConfig;
-import java.util.Optional;
 import org.eclipse.paho.client.mqttv3.MqttClient;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
+import java.util.Optional;
+
 @Component
 public class HassAirPurifierEventHandler extends HassDeviceEventHandler<AirPurifierDevice> {
     private final static String HASS_COMPONENT = "fan";
+    private final static String TOPIC_SPEED = "speed";
+    private final static String TOPIC_PRESET = "preset";
 
     private final AirPurifierDeviceApi api;
 
     public HassAirPurifierEventHandler(
             final MqttClient mqtt,
             final DirigeraApi api,
-            @Value("${dirigera.mqtt.hass.prefix:homeassistant}")
-            final String topicPrefix,
+            @Value("${dirigera.mqtt.hass.prefix:homeassistant}") final String topicPrefix,
             final ObjectMapper objectMapper) {
         super(mqtt, api, AirPurifierDevice.class, topicPrefix, objectMapper);
 
@@ -36,25 +38,24 @@ public class HassAirPurifierEventHandler extends HassDeviceEventHandler<AirPurif
     @Override
     protected void onDeviceCreated(final AirPurifierDevice device) {
         final AirPurifierConfig config;
+        final AirPurifierFanMode[] presetModes = new AirPurifierFanMode[]{AirPurifierFanMode.AUTO, AirPurifierFanMode.LOW, AirPurifierFanMode.MEDIUM, AirPurifierFanMode.HIGH};
 
         config = new AirPurifierConfig();
         config.unique_id = config.object_id = device.id;
         config.name = getDefaultName(device);
         config.device = this.getDeviceConfig(device);
         config.command_topic = this.getTopic(device, HASS_COMPONENT, TOPIC_SET);
+        config.percentage_state_topic = this.getTopic(device, HASS_COMPONENT, TOPIC_SPEED);
+        config.preset_mode_command_topic = this.getTopic(device, HASS_COMPONENT, TOPIC_PRESET, TOPIC_SET);
+        config.preset_mode_state_topic = this.getTopic(device, HASS_COMPONENT, TOPIC_PRESET, TOPIC_STATE);
         config.state_topic = this.getTopic(device, HASS_COMPONENT, TOPIC_STATE);
         config.payload_off = this.toJSON(AirPurifierFanMode.OFF);
         config.payload_on = this.toJSON(AirPurifierFanMode.ON);
-        config.payload_low = this.toJSON(AirPurifierFanMode.LOW);
-        config.payload_medium = this.toJSON(AirPurifierFanMode.MEDIUM);
-        config.payload_high = this.toJSON(AirPurifierFanMode.HIGH);
-        config.payload_auto = this.toJSON(AirPurifierFanMode.AUTO);
-        config.state_off =  this.toJSON(AirPurifierFanMode.OFF);
-        config.state_on =  this.toJSON(AirPurifierFanMode.ON);
-        config.state_low =  this.toJSON(AirPurifierFanMode.LOW);
-        config.state_medium = this.toJSON(AirPurifierFanMode.MEDIUM);
-        config.state_high = this.toJSON(AirPurifierFanMode.HIGH);
-        config.state_auto = this.toJSON(AirPurifierFanMode.AUTO);
+        config.preset_modes = this.toJSON(presetModes);
+        config.state_off = this.toJSON(AirPurifierFanMode.OFF);
+        config.state_on = this.toJSON(AirPurifierFanMode.ON);
+        config.preset_mode_value_template = "{{value}}";
+        config.percentage_value_template = "{{value}}";
         config.value_template = "{{value}}";
 
         config.availability = new DeviceAvailability();
@@ -67,6 +68,10 @@ public class HassAirPurifierEventHandler extends HassDeviceEventHandler<AirPurif
                 this.getTopic(device, HASS_COMPONENT, TOPIC_SET),
                 AirPurifierFanMode.class,
                 status -> this.setDeviceStatus(device, status));
+        this.subscribe(
+                this.getTopic(device, HASS_COMPONENT, TOPIC_PRESET, TOPIC_SET),
+                AirPurifierFanMode.class,
+                status -> this.setPresetMode(device, status));
 
         this.onDeviceStateChanged(device);
     }
@@ -75,10 +80,18 @@ public class HassAirPurifierEventHandler extends HassDeviceEventHandler<AirPurif
         this.api.setFanMode(device, state).block();
     }
 
+    protected void setPresetMode(final AirPurifierDevice device, final AirPurifierFanMode state) {
+        this.api.setFanMode(device, state).block();
+    }
+
     @Override
     protected void onDeviceStateChanged(final AirPurifierDevice device) {
         getState(device).ifPresent(state ->
                 this.publish(this.getTopic(device, HASS_COMPONENT, TOPIC_STATE), state));
+        getPreset(device).ifPresent(preset ->
+                this.publish(this.getTopic(device, HASS_COMPONENT, TOPIC_PRESET, TOPIC_STATE), preset));
+        getSpeed(device).ifPresent(speed ->
+                this.publish(this.getTopic(device, HASS_COMPONENT, TOPIC_SPEED), speed));
         getAvailability(device).ifPresent(s ->
                 this.publish(this.getTopic(device, HASS_COMPONENT, TOPIC_AVAILABILITY), s));
     }
@@ -92,8 +105,26 @@ public class HassAirPurifierEventHandler extends HassDeviceEventHandler<AirPurif
 
     public static Optional<AirPurifierFanMode> getState(final AirPurifierDevice device) {
         return Optional.of(device)
-                .map(d->d.attributes)
-                .map(a->a.state)
-                .map(s->s.fanMode);
+                .map(d -> d.attributes)
+                .map(a -> a.state)
+                .map(s -> switch (s.fanMode) {
+                    case LOW, MEDIUM, HIGH, AUTO, ON -> AirPurifierFanMode.ON;
+                    case OFF -> AirPurifierFanMode.OFF;
+                });
+    }
+
+    public static Optional<AirPurifierFanMode> getPreset(final AirPurifierDevice device) {
+        return Optional.of(device)
+                .map(d -> d.attributes)
+                .map(a -> a.state)
+                .filter(a -> !AirPurifierFanMode.OFF.equals(a.fanMode) && !AirPurifierFanMode.ON.equals(a.fanMode))
+                .map(s -> s.fanMode);
+    }
+
+    public static Optional<Integer> getSpeed(final AirPurifierDevice device) {
+        return Optional.of(device)
+                .map(d -> d.attributes)
+                .map(a -> a.state)
+                .map(s -> s.motorState);
     }
 }
--- dirigera-client-mqtt/src/main/java/de/dvdgeisler/iot/dirigera/client/mqtt/hass/model/airpurifier/AirPurifierConfig.java
+++ dirigera-client-mqtt/src/main/java/de/dvdgeisler/iot/dirigera/client/mqtt/hass/model/airpurifier/AirPurifierConfig.java
@@ -6,47 +6,54 @@ import de.dvdgeisler.iot.dirigera.client.mqtt.hass.model.DeviceAvailability;
 
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public class AirPurifierConfig {
-  public String object_id;
-  public String unique_id;
-  public String name;
-  public Device device;
-  public String payload_off;
-  public String payload_on;
-  public String payload_low;
-  public String payload_medium;
-  public String payload_high;
-  public String payload_auto;
-  public String command_topic;
-  public String state_off;
-  public String state_on;
-  public String state_low;
-  public String state_medium;
-  public String state_high;
-  public String state_auto;
-  public String state_topic;
-  public String value_template;
-  public DeviceAvailability availability;
+    public String object_id;
+    public String unique_id;
+    public String name;
+    public Device device;
+    public boolean optimistic;
+    public String payload_off;
+    public String payload_on;
+    public String command_topic;
+    public String state_off;
+    public String state_on;
+    public String state_topic;
+    public String value_template;
+    public String percentage_state_topic;
+    public String percentage_value_template;
+    public String preset_mode_command_template;
+    public String preset_mode_command_topic;
+    public String preset_mode_state_topic;
+    public String preset_mode_value_template;
+    public String preset_modes;
+    public DeviceAvailability availability;
 
-  public AirPurifierConfig(String object_id, String unique_id, String name, Device device,
-      String payload_low, String payload_auto, String payload_medium, String command_topic,
-      String state_low, String state_auto, String state_medium, String state_topic,
-      String value_template, DeviceAvailability availability) {
-    this.object_id = object_id;
-    this.unique_id = unique_id;
-    this.name = name;
-    this.device = device;
-    this.payload_low = payload_low;
-    this.payload_auto = payload_auto;
-    this.payload_medium = payload_medium;
-    this.command_topic = command_topic;
-    this.state_low = state_low;
-    this.state_auto = state_auto;
-    this.state_medium = state_medium;
-    this.state_topic = state_topic;
-    this.value_template = value_template;
-    this.availability = availability;
-  }
+    public AirPurifierConfig(String object_id, String unique_id, String name, Device device, boolean optimistic,
+                             String payload_off, String payload_on, String command_topic, String state_off,
+                             String state_on, String state_topic, String value_template, String percentage_state_topic,
+                             String percentage_value_template, String preset_mode_command_template, String preset_mode_command_topic,
+                             String preset_mode_state_topic, String preset_mode_value_template, String preset_modes, DeviceAvailability availability) {
+        this.object_id = object_id;
+        this.unique_id = unique_id;
+        this.name = name;
+        this.device = device;
+        this.optimistic = optimistic;
+        this.payload_off = payload_off;
+        this.payload_on = payload_on;
+        this.command_topic = command_topic;
+        this.state_off = state_off;
+        this.state_on = state_on;
+        this.state_topic = state_topic;
+        this.value_template = value_template;
+        this.percentage_state_topic = percentage_state_topic;
+        this.percentage_value_template = percentage_value_template;
+        this.preset_mode_command_template = preset_mode_command_template;
+        this.preset_mode_command_topic = preset_mode_command_topic;
+        this.preset_mode_state_topic = preset_mode_state_topic;
+        this.preset_mode_value_template = preset_mode_value_template;
+        this.preset_modes = preset_modes;
+        this.availability = availability;
+    }
 
-  public AirPurifierConfig() {
-  }
+    public AirPurifierConfig() {
+    }
 }
